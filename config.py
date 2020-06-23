884adfb831c742aa6d5e75cdfd55258c6fab6844
ssayic <66877343+ssayic@users.noreply.github.com>
2020-06-23 21:57:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,22 +5,12 @@ class Config(object):
 
     ABS_PATH = '/'
 
-    # postgres
-    """
-    PYTHONGRID_DB_HOSTNAME = 'localhost'
-    PYTHONGRID_DB_NAME = 'sampledb'
-    PYTHONGRID_DB_USERNAME = 'root'
-    PYTHONGRID_DB_PASSWORD = 'root'
-    PYTHONGRID_DB_TYPE = 'postgres+psycopg2'
-    PYTHONGRID_DB_SOCKET = ''
-    PYTHONGRID_DB_CHARSET = 'utf-8'
-    """
     
     # mysql 
     PYTHONGRID_DB_HOSTNAME = ''
-    PYTHONGRID_DB_NAME = 'sampledb'
+    PYTHONGRID_DB_NAME = 'sys'
     PYTHONGRID_DB_USERNAME = 'root'
-    PYTHONGRID_DB_PASSWORD = 'root'
+    PYTHONGRID_DB_PASSWORD = 'Selim.1083'
     PYTHONGRID_DB_TYPE = 'mysql+pymysql'
     PYTHONGRID_DB_SOCKET = '/Applications/MAMP/tmp/mysql/mysql.sock'
     PYTHONGRID_DB_CHARSET = 'utf-8'
